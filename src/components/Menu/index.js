c7a511b522c323268495f46ec0a9cc52f1ceed95
jvpelai <jv.pelai@gmail.com>
2020-07-29 21:56:47 -0300

    added forms

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,7 +8,7 @@ import Button from '../Button'
 function Menu(){
   return(
     <nav className="Menu">
-      <Link href="/">
+      <Link to="/">
         <img className = "Logo" src={Logo} alt="DroidFlix"></img>
       </Link>
 
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,25 +1,91 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import {Link} from 'react-router-dom'
 import PageDefault from '../../../components/PageDefault'
 
 function CadastroCategoria(){
+  const [categorias,setCategorias] = useState(['Teste']);
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#000',
+  }
+  const [values,setValues] = useState('Valor inicial');
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    })
+  }
+
+  function handleChange(infosDoEvento){
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
+    );
+  }
   return(
     <PageDefault>
-      <h1> Cadastro de categoria </h1>
+      <h1> Cadastro de categoria: {values.nome}</h1>
 
-      <form>
+      <form  style={{background: values.cor}} onSubmit = {function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        console.log('tentastes enviar o form')
+        setCategorias([
+          ...categorias,
+          values.nome
+        ]);
+      }}>
+        <div>
 
         <label>
           Nome da Categoria:
           <input
             type="text"
+            name="nome"
+            value = {values.nome}
+            onChange = {handleChange}
           />
         </label>
+        </div>
+        <br></br>
+        <div>
+        <label>
+          Descrição: <br></br>
+          <textarea
+            type="text"
+            name="descrição"
+            value = {values.descricao}
+            onChange = {handleChange}
+          />
+        </label>
+        </div>
+        <br></br>
+        <div>
+        <label>
+          Cor: 
+          <input 
+            type="color"
+            name = "cor"
+            value = {values.cor}
+            onChange = {handleChange}
+          />
+        </label>
+        </div>
 
         <button>
           Cadastrar
         </button>
       </form>
+      <ul>
+        {categorias.map((categoria,indice) =>{
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
 
 
       
